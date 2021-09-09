d5f447cbd1c01035dff19d72ad2dc5525b85757b
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-09-09 16:33:55 -0500

    Usar rango de urna para crear urnas
    Co-Authored-By: Archanaver <42619971+Archanaver@users.noreply.github.com>

--- Actividad_3/main.c
+++ Actividad_3/main.c
@@ -15,8 +15,33 @@ struct Archivo{
 
 };
 
-int max(size_t val1, size_t val2){
+size_t max(size_t val1, size_t val2){
     return val1 > val2 ? val1: val2;
+}
+
+void recursiveReaderFalso(char *path, struct Vector* listaTamaños){
+    struct Archivo* archivo1 = malloc(sizeof(struct Archivo));
+    archivo1->cantidad = 1;
+    push_back(listaTamaños, archivo1);
+    struct Archivo* archivo2 = malloc(sizeof(struct Archivo));
+    archivo2->cantidad = 2;
+    push_back(listaTamaños, archivo2);
+    struct Archivo* archivo3 = malloc(sizeof(struct Archivo));
+    archivo3->cantidad = 90;
+    push_back(listaTamaños, archivo3);
+    struct Archivo* archivo4 = malloc(sizeof(struct Archivo));
+    archivo4->cantidad = 2;
+    push_back(listaTamaños, archivo4);
+    struct Archivo* archivo5 = malloc(sizeof(struct Archivo));
+    archivo5->cantidad = 2;
+    push_back(listaTamaños, archivo5);
+    struct Archivo* archivo6 = malloc(sizeof(struct Archivo));
+    archivo6->cantidad = 11;
+    push_back(listaTamaños, archivo6);
+    struct Archivo* archivo7 = malloc(sizeof(struct Archivo));
+    archivo7->cantidad = 19;
+    push_back(listaTamaños, archivo7);
+    listaTamaños->maxElement = 90;
 }
 
 void recursiveReader(char *path, struct Vector* listaTamaños){
@@ -32,11 +57,9 @@ void recursiveReader(char *path, struct Vector* listaTamaños){
         {
             struct stat file_attr;
             stat(dirp->d_name, &file_attr);
-            //printf("File: '%s' Size: %lld bytes\n",dirp->d_name, file_attr.st_size);
             struct Archivo* archivo = malloc(sizeof(struct Archivo));
             archivo->cantidad = file_attr.st_size;
-            push_back(listaTamaños, &archivo);
-            //printf("----File: '%zu' \n",archivo->cantidad);
+            push_back(listaTamaños, archivo);
             listaTamaños->maxElement = max(listaTamaños->maxElement, archivo->cantidad);
             strcpy(tempPath, path);
             strcat(tempPath, "/");
@@ -48,19 +71,15 @@ void recursiveReader(char *path, struct Vector* listaTamaños){
     free(tempPath);
 }
 
-void llenarBuckets(int* buckets, struct Vector* listaTamaños){
+void llenarBuckets(int* buckets, struct Vector* listaTamaños, size_t rango){
     struct Nodo* nodoInicial = begin(listaTamaños);
-    long double rango = listaTamaños->maxElement / 10 + 1;
-    //printf("gap %Lf\n", rango);
     while(nodoInicial != NULL){
-        size_t size = *((size_t*) nodoInicial->valor);
-        size_t indiceBucket = (size / rango);
-        //printf("Size: %zu  indice: %zu\n", size, indiceBucket);
+        size_t fileSize = *((size_t*) nodoInicial->valor);
+        size_t indiceBucket = fileSize / rango;
         buckets[indiceBucket]++;
         nodoInicial = next(nodoInicial);
     }
 }
-
 
 int main(int argc, char * const * argv){
     char* dvalue = NULL;
@@ -82,10 +101,12 @@ int main(int argc, char * const * argv){
     }
 
     struct Vector listaTamaños;
-    recursiveReader(dvalue, &listaTamaños);
+    recursiveReaderFalso(dvalue, &listaTamaños);
     printf("Hay %zu archivos y el tamaño máximo es %zu\n", listaTamaños.length, listaTamaños.maxElement);
-    int buckets[10] = {};
-    llenarBuckets(buckets, &listaTamaños);
+    size_t anchuraUrna = 15;
+    size_t numberOfBuckets = listaTamaños.maxElement / anchuraUrna + 1;
+    int* buckets = (int*) malloc(sizeof(int) * numberOfBuckets);
+    llenarBuckets(buckets, &listaTamaños, anchuraUrna);
     for(int i = 0; i < 10; i++)
       printf("%d ", buckets[i]);
     puts("");
