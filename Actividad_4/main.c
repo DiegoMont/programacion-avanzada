4c143cfcb03032369cf2f0d299861816abbac7ed
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-09-17 16:32:15 -0500

    Corrección formato de scan

--- Actividad_4/main.c
+++ Actividad_4/main.c
@@ -14,88 +14,6 @@ int randomNum(int max){
     return rand() % max;
 }
 
-/* int movimientoTortuga(int num){
-    switch (num)
-    {
-        case 1 ... 5:
-            return 3;
-        case 6 ... 7:
-            return -6;
-        default:
-            return 1;
-    }   
-}
-
-int movimientoLiebre(int num){
-    switch (num)
-    {
-        case 1 ... 2:
-            return 0;
-        case 3 ... 4:
-            return 9;
-        case 5:
-            return -12;
-        case 6 ... 8:
-            return 1;
-        default:
-            return -2;
-    }   
-}
-
-
-void posicionTortuga(int **fd){
-    int pos_tortuga = 0;
-    while(pos_tortuga < 70){
-        close(fd[0][0]);
-        close(fd[1][0]);
-        close(fd[1][1]);
-        int temp = randomNum(1,10);
-        pos_tortuga += movimientoTortuga(temp);
-        //printf("random:%d  pos:%d\n", temp, pos_tortuga);
-        write(fd[0][1], &pos_tortuga, sizeof(int));
-        sleep(1);
-        
-    }
-}
-
-void posicionLiebre(int **fd){
-    int pos_liebre = 0;
-    while(pos_liebre < 70){
-        close(fd[0][0]);
-        close(fd[0][1]);
-        close(fd[1][0]);
-        int temp = randomNum(1,10);
-        pos_liebre += movimientoLiebre(temp);
-        //printf("random:%d  pos:%d\n", temp, pos_tortuga);
-        write(fd[1][1], &pos_liebre, sizeof(int));
-        sleep(1);
-        
-    }
-}
-
-void printerTablero(char *tablero, int casillas, int pos_tortuga, char t){
-    char *final = tablero + casillas;
-    char *tortuga = tablero + pos_tortuga;
-    char*aux = tablero;
-    if(pos_tortuga<0){
-         printf(" %c ", t);
-         aux++;
-         for(; aux < final; aux++)
-              printf("_ ");      
-    }else{
-        for(; aux < final; aux++){
-        if(aux == tortuga)
-            printf(" %c ", t);
-        else
-            printf("_ ");   
-        }
-    }
-
-    printf("\n");
-    printf("\n");
-    
-} */
-
 void imprimirCarrera(){
     for(int i = 0; i < LARGO_PISTA; i++){
         int liebreSeEncuentraAqui = posicionLiebre == i;
@@ -131,7 +49,27 @@ void imprimirGanador(){
 }
 
 void calcularPosicionLiebre(int fileDescriptor){
-    return;
+    int probabilidad = randomNum(10);
+    int movimientoARealizar;
+    if(probabilidad < 2)
+        movimientoARealizar = 0;
+    else if(probabilidad < 4)
+        movimientoARealizar = 9;
+    else if(probabilidad < 5)
+        movimientoARealizar = -12;
+    else if(probabilidad < 8)
+        movimientoARealizar = 1;
+    else
+        movimientoARealizar = -2;
+    int nuevaPosicion = posicionLiebre + movimientoARealizar;
+    if(nuevaPosicion < 0)
+        nuevaPosicion = 0;
+
+    FILE * file;
+    file = fdopen(fileDescriptor, "w");
+    fprintf(file, "%d", nuevaPosicion);
+    printf("Liebre = %d\n", nuevaPosicion);
+    fclose(file);
 }
 
 void calcularPosicionTortuga(int fileDescriptor){
@@ -159,16 +97,20 @@ void leerPipesYActualizarPosiciones(int* pipeLiebre, int* pipeTortuga){
     close(*(pipeTortuga + 1));
 
     FILE * file;
-    int c;
-    
+    int posicionAsChar;
+    int posicionAsInt;
+    const int VALOR_ASCII_DE_CERO = 48;
+
     file = fdopen(*pipeLiebre, "r");
-    while ( (c = fgetc(file)) != EOF )
-        posicionLiebre = c;
+    while ( (posicionAsChar = fgetc(file)) != EOF )
+        posicionAsInt = posicionAsChar - VALOR_ASCII_DE_CERO;
+    posicionLiebre = posicionAsInt;
     fclose(file);
 
     file = fdopen(*pipeTortuga, "r");
-    while ( (c = fgetc(file)) != EOF )
-        posicionTortuga = c;
+    while ( (posicionAsChar = fgetc(file)) != EOF )
+        posicionAsInt = posicionAsChar - VALOR_ASCII_DE_CERO;
+    posicionTortuga = posicionAsInt;
     fclose(file);
 }
 
@@ -182,8 +124,8 @@ int main(){
     pipe(pipeLiebre);
 
     //Comienza la carrera
-    posicionTortuga = 10;    
-    posicionLiebre = 10;
+    posicionTortuga = 0;    
+    posicionLiebre = 0;
     puts("BANG!!!!!\nY ARRANCAN!!!!!");
     clock_t ultimaActualizacionPosiciones = clock();
     while(!terminoLaCarrera()){
@@ -194,7 +136,7 @@ int main(){
             if (pidLiebre == -1)
                 ;
             else if(pidLiebre == 0){
-                close(*(pipeLiebre + 0));
+                close(*(pipeLiebre));
                 calcularPosicionLiebre(*(pipeLiebre + 1));
                 exit(0);
             } else {
@@ -202,11 +144,11 @@ int main(){
                 if(pidTortuga == -1)
                     ;
                 else if(pidTortuga == 0) {
-                    close(*(pipeTortuga + 0));
+                    close(*(pipeTortuga));
                     calcularPosicionTortuga(*(pipeTortuga + 1));
                     exit(0);
                 } else {
-                    //leerPipesYActualizarPosiciones(pipeLiebre, pipeTortuga);
+                    leerPipesYActualizarPosiciones(pipeLiebre, pipeTortuga);
                 }
             }
             imprimirCarrera();
