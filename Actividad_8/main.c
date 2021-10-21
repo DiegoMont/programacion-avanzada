046464af89a74d7a625c010e48f7737e8101fea1
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-10-20 22:42:41 -0500

    Algoritmo en pseudocódigo

--- Actividad_8/main.c
+++ Actividad_8/main.c
@@ -7,7 +7,20 @@
 
 #include "main.h"
 
+/*
+Hay clientesGeneralesFormados, clientesEmpresarialesFormados
+En un arreglo guardamos el estado de los cajeros, los primeros tres cajeros son empresariales el resto de las operaciones.
+El estado de los cajeros es disponible u ocupado
+Tenemos otro semáforo que indica cuántos cajerosDisponibles tenemos
+Cuando un cajero está listo para atender incrementa cajerosDisponibles:
+  Los clientes checan cuál cajero está disponible:
+    Si es empresarial y son empresariales se van a ese cajero y lo marcan como ocupado
+    Si es empresarial y son generales lo toman únicamente si no hay clientes empresariales esperando
+    Si es general y son generales lo toman
+    Si es general y son empresariales GG
+*/
+
 int main(){
-    
+    srand(time(NULL));
 }
 
