df9d3f5ba170f6cec63f267b68b69d8c4bab45eb
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-11-29 17:18:14 -0600

    Testeo exitoso de algoritmo
    Co-Authored-By: Archana Verma <42619971+Archanaver@users.noreply.github.com>

--- Proyecto_final/main.c
+++ Proyecto_final/main.c
@@ -1,15 +1,25 @@
+//#include <openssl/sha.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <openssl/sha.h>
+#include <string.h>
 
 #include "main.h"
 
 const int PASSWORD_SIZE = 5;
 
+char const alphabet[] = {
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+};
+const size_t ALPHABET_LENGTH = 26;
+const size_t HASH_LENGTH = 32;
+
 int main(){
     char* userPassword = getPassword(PASSWORD_SIZE);
+    unsigned char userHash[32];
+    //SHA256(userPassword, PASSWORD_SIZE, userHash);
     puts(userPassword);
-    unsigned char* userHash = SHA256(userPassword, PASSWORD_SIZE, 0);
+    puts(userHash);
+    bruteForcePassword(userHash);
     // Cleaning up
     free(userPassword);
 }
@@ -17,8 +27,43 @@ int main(){
 char* getPassword(int passwordSize){
     printf("Ingrese una contraseña de %d caracteres que contenga solo letras minúsculas: ", passwordSize);
     char* password = (char*) malloc(passwordSize * sizeof(char));
-    scanf("%s", password);
+    strcpy(password, "aaaaa");
+    //scanf("%s", password);
     return password;
 }
 
+void bruteForcePassword(unsigned char* userHash){
+    for(size_t i = 0; i < ALPHABET_LENGTH; i++){
+        char testPassword[PASSWORD_SIZE];
+        testPassword[0] = *(alphabet + i);
+        for(size_t j = 0; j < ALPHABET_LENGTH; j++){
+            testPassword[1] = *(alphabet + j);
+            for(size_t k = 0; k < ALPHABET_LENGTH; k++){
+                testPassword[2] = *(alphabet + k);
+                for(size_t l = 0; l < ALPHABET_LENGTH; l++){
+                    testPassword[3] = *(alphabet + l);
+                    for(size_t m = 0; m < ALPHABET_LENGTH; m++){
+                        testPassword[4] = *(alphabet + m);
+                        /* unsigned char testHash[32];
+                        SHA256(testPassword, PASSWORD_SIZE, testHash);
+                        int areHashesEqual = compareHashes(testHash, userHash);
+                        if(areHashesEqual){
+                            puts(testPassword);
+                            return;
+                        } */
+                    }
+                }
+            }
+        }
+    }
+}
 
+void tryPassword(char* testPassword, unsigned char* userHash){}
+
+int compareHashes(unsigned char* hash1, unsigned char* hash2){
+    for(size_t i = 0; i < HASH_LENGTH; i++){
+        if(*(hash1 + i) != *(hash2 + i))
+            return 0;
+    }
+    return 1;
+}
--- Proyecto_final/main.h
+++ Proyecto_final/main.h
@@ -1,1 +1,3 @@
+void bruteForcePassword(unsigned char* userHash);
+int compareHashes(unsigned char* hash1, unsigned char* hash2);
 char* getPassword(int passwordSize);
