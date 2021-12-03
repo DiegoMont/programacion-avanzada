5c25530a332f085071937f84b945aef31d58e21e
DiegoMont <31603020+DiegoMont@users.noreply.github.com>
2021-12-03 03:39:57 -0600

    Successful MPI?

--- Proyecto_final/main.c
+++ Proyecto_final/main.c
@@ -1,23 +1,37 @@
-// gcc singleThreadMain.c -fopenmp -lssl -lcrypto
+// mpicc main.c -fopenmp
+// mpirun -f machines -n 2 ./a.out
 
+#include "mpi.h"
 #include <omp.h>
 #include "main.h"
 
-int main(){
+static int startFrom;
+
+int main(int argc, char** argv){
+    int numtasks, id, rc;
+    rc = MPI_Init(&argc, &argv);
+    if(rc != MPI_SUCCESS){
+        puts("Error con MPI");
+        MPI_Abort(MPI_COMM_WORLD, rc);
+    }
+    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &id);
     char* userPassword = getPassword(PASSWORD_SIZE);
     unsigned char userHash[HASH_LENGTH];
     //SHA256(userPassword, strlen(userPassword), userHash);
     puts(userPassword);
+    startFrom = id == 0 ? 0: 1;
     bruteForcePassword(userHash);
 
     // Cleaning up
     free(userPassword);
+    MPI_Finalize();
 }
 
 void bruteForcePassword(unsigned char* userHash){
     size_t possibleCombinations = elevateToPow(ALPHABET_LENGTH, PASSWORD_SIZE);
     #pragma omp parallel for shared(possibleCombinations)
-    for(int combination = 0; combination < possibleCombinations; combination++){
+    for(int combination = startFrom; combination < possibleCombinations; combination+=2){
         testPassword(userHash, combination);
     }
 }
